c60e24bb27fa11d8f9641ffcb2d686e83cc405f2
aracel1234 <nestovaa@gmail.com>
2025-05-09 21:07:17 +0700

    Update frontend_streamlit.py

--- app/frontend_streamlit.py
+++ app/frontend_streamlit.py
@@ -1,1 +1,17 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from app.tfidf_search import TfidfRetriever
+from app.llama_utils import generate_answer
 
+retriever = TfidfRetriever("data_buku.csv")
+app = FastAPI()
+
+class Question(BaseModel):
+    query: str
+
+@app.post("/ask")
+def ask_q(data: Question):
+    docs = retriever.retrieve(data.query)
+    context = " ".join(docs['sinopsis'].values.tolist())
+    answer = generate_answer(context, data.query)
+    return {"answer": answer}
