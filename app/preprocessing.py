9a0f28d301f08ed294e789844435a2555e0233d5
aracel1234 <nestovaa@gmail.com>
2025-05-09 21:05:54 +0700

    Update preprocessing.py

--- app/preprocessing.py
+++ app/preprocessing.py
@@ -1,1 +1,12 @@
+import nltk
+import re
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
+nltk.download('stopwords')
+
+def clean_text(text):
+    text = re.sub(r'[^a-zA-Z\s]', '', text.lower())
+    stop_words = set(stopwords.words('indonesian'))
+    stemmer = PorterStemmer()
+    return ' '.join([stemmer.stem(w) for w in text.split() if w not in stop_words])
